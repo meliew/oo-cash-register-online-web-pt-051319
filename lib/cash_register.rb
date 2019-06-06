cef017d0cf72a4a28038271597f0809fea7b7c14
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-06 23:52:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
 
   def apply_discount
     @total -= (@discount/100.0) * @total
-    "After the discount, the total comes to $#{@total}."
+    "After the discount, the total comes to $#{@total.to_i}."
   end
 
 end
